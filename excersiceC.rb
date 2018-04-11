91296ef90733959da08b7fad45c0465d97762731
Luis Tejero <luisfaridta@gmail.com>
2018-04-11 21:34:38 +0100

    added comments and corrected typos

--- excersiceC.rb
+++ excersiceC.rb
@@ -15,18 +15,27 @@ united_kingdom = [
     capital: "London"
   }
 ]
+# Change the capital of Wales from "Swansea" to "Cardiff".
+
 united_kingdom[1][:capital]= "Cardiff"
+
+# Create a Hash for Northern Ireland and add it to the united_kingdom array (The capital is Belfast, and the population is 1,811,000).
+
 united_kingdom.push({
   name: "Northern Ireland",
   population: 1811000,
   capital: "Belfast"
   })
+
 p united_kingdom
 
-for capital in united_kingdom
-p capital[:capital]
+# Use a loop to print the names of all the countries in the UK.
+
+for countries in united_kingdom
+p countries[:name]
 end
 
+# Use a loop to find the total population of the UK.
 total = 0
 for population in united_kingdom
     total+= population[:population]
