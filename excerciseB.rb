8e0f7bea240e8b9044f3cb86342518c4175f5848
Luis Tejero <luisfaridta@gmail.com>
2018-04-11 21:24:40 +0100

    added comments excercise B

--- excerciseB.rb
+++ excerciseB.rb
@@ -54,34 +54,54 @@ users = {
   }
 }
 
+# Get Jonathan's Twitter handle (i.e. the string "jonnyt")
 p users["Jonathan"][:twitter]
+# Get Erik's hometown
+p users["Erik"][:home_town]
+# Get the array of Erik's lottery numbers
+p users["Erik"][:lottery_numbers]
+# Get the type of Avril's pet Monty
 p users["Avril"][:pets][0][:species]
-erikslottery =users["Erik"][:lottery_numbers]
-p erikslottery.min
+
+# Get the smallest of Erik's lottery numbers
+
+# erikslottery =users["Erik"][:lottery_numbers]
+# p erikslottery.min
+
+p users["Erik"][:lottery_numbers].min
+
+# Return an array of Avril's lottery numbers that are even
 
 p avrilslottery =users["Avril"][:lottery_numbers].select { |num|  num.even?  }
+
 #longestway
-# avrilsArray = [12, 14, 33, 38, 9, 25]
 # def even_array(array)
 #   lottery_array = []
 # for numbers in array
-#   if numbers%2 == 0
+#   if (numbers%2 == 0)
 #     lottery_array.push(numbers)
 #   end
 # end
 # return lottery_array
 # end
-# p even_array(avrilsArray)
+# p even_array(users["Avril"][:lottery_numbers])
+
+# Erik is one lottery number short! Add the number 7 to be included in his lottery numbers
 users["Erik"][:lottery_numbers].push(7)
 p users["Erik"][:lottery_numbers]
+
+# Change Erik's hometown to Edinburgh
 users["Erik"][:home_town]= "Edinburgh"
 p users["Erik"][:home_town]
+
+# Add a pet dog to Erik called "Fluffy"
 users["Erik"][:pets].push({
   :name => "Fluffy",
   :species => "doggo"
   })
 p users["Erik"][:pets]
 
+# Add another person to the users hash
 users["Luis"]= {
   :twitter => "luist17",
   :lottery_numbers => [12, 14, 33, 38, 9, 25],
@@ -95,14 +115,3 @@ users["Luis"]= {
 }
 
 p users
-
-# Get Jonathan's Twitter handle (i.e. the string "jonnyt")
-# Get Erik's hometown
-# Get the array of Erik's lottery numbers
-# Get the type of Avril's pet Monty
-# Get the smallest of Erik's lottery numbers
-# Return an array of Avril's lottery numbers that are even
-# Erik is one lottery number short! Add the number 7 to be included in his lottery numbers
-# Change Erik's hometown to Edinburgh
-# Add a pet dog to Erik called "Fluffy"
-# Add another person to the users hash
